f1a51495e33c8d2d0ea16305e7172064101187b8
gloire <gloirengandu29@gmail.com>
2023-02-20 01:58:57 +0100

    uixAppCalcFin

--- app/src/main/java/com/example/calculatriceapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatriceapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculatriceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view:View){
+        Toast.makeText(this,"le Bouton est cliquer",Toast.LENGTH_LONG).show()
     }
 }
