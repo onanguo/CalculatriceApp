a8859fc58fd21eaedd42e8f005e4268b3076be8a
gloire <gloirengandu29@gmail.com>
2023-02-20 02:18:48 +0100

    implementation de Onclick et onClear

--- app/src/main/java/com/example/calculatriceapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatriceapp/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.calculatriceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput : TextView? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         tvInput = findViewById(R.id.tvInput)
     }
     fun onDigit(view:View){
-        Toast.makeText(this,"le Bouton est cliquer",Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+    }
+    fun onClear(view: View){
+        tvInput?.text =""
     }
 }
